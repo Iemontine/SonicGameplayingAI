6f01993ce127cea3ac65912be2a47492850f2fdf
Iemontine <darrollsaddi@gmail.com>
2024-05-22 22:22:40 -0700

    separated code, started training Sonic models, began bayesian optimization integration

---
+++ helpers.py
@@ -0,0 +1,89 @@
+from stable_baselines3.common.vec_env import SubprocVecEnv, DummyVecEnv
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.callbacks import BaseCallback
+from stable_baselines3.common.env_checker import check_env 
+
+class ActionMappingWrapper(gym.ActionWrapper):
+	def __init__(self, env, action_mapping):
+		super(ActionMappingWrapper, self).__init__(env)
+		self.action_mapping = action_mapping
+		self.action_space = gym.spaces.Discrete(len(action_mapping))
+
+	def action(self, action):
+		return self.action_mapping[action]
+
+class RewardWrapper(gym.Wrapper):
+	def __init__(self, env):
+		super(RewardWrapper, self).__init__(env)
+		self.env.reset()
+		_, _, _, _, info = self.env.step(env.action_space.sample())
+		self.start_pos = info["x"]
+		self.start_score = info["score"]
+		self.start_rings = info["rings"]
+		self.start_lives = info["lives"]
+		self.env.reset()
+		self.progress = self.start_pos
+		self.curr_score = self.start_score
+		self.curr_rings = self.start_rings
+		self.curr_lives = self.start_lives
+
+	def step(self, action):
+		obs, _, terminated, truncated, info = self.env.step(action)
+		reward = min(max((info['x'] - self.progress), 0), 2)
+		self.progress = max(info['x'], self.progress)
+		# reward += (info["score"] - self.curr_score) / 40
+		# self.curr_score = info["score"]
+		if info["lives"] < 3:
+			return obs, -5, False, True, info
+		return obs, reward / 10, terminated, truncated, info
+
+class FramePreprocessingWrapper(gym.ObservationWrapper):
+	def __init__(self, env):
+		super(FramePreprocessingWrapper, self).__init__(env)
+		self.observation_space = gym.spaces.Box(low=0, high=255, shape=(84, 84, 1), dtype=np.uint8)
+
+	def observation(self, obs):
+		gray = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+		resized = cv2.resize(gray, (84, 84), interpolation=cv2.INTER_AREA)
+		return np.expand_dims(resized, axis=-1)
+
+class FrameSkipper(gym.Wrapper):
+	def __init__(self, env, skip=4):
+		super(FrameSkipper, self).__init__(env)
+		self.skip = skip
+
+	def step(self, action):
+		total_reward = 0.0
+		done = None
+		obs = None
+		info = {}
+		for i in range(self.skip):
+			obs, reward, terminated, truncated, info = self.env.step(action)
+			total_reward += reward
+			if terminated or truncated:
+				break
+		return obs, total_reward, terminated, truncated, info
+
+	def reset(self, **kwargs):
+		return self.env.reset(**kwargs)
+	
+class Callback(BaseCallback):
+	def __init__(self, verbose=0):
+		super(Callback, self).__init__(verbose)
+		self.losses = []
+
+	def _on_step(self) -> bool:
+		if 'rewards' in self.locals and np.any(self.locals['rewards'] > 0):
+			print(f"Reward on {self.num_timesteps}: {self.locals['rewards']}")
+		elif 'rewards' in self.locals and np.any(self.locals['rewards'] < 0):
+			print(f"Loss on {self.num_timesteps}: {self.locals['rewards']}")
+		return True
+
+# Multiprocessing
+def make_env():
+	env = retro.make('SonicTheHedgehog-Genesis')
+	env = RewardWrapper(env)
+	env = FramePreprocessingWrapper(env)
+	env = FrameSkipper(env, skip=4)
+	env = ActionMappingWrapper(env, ACTION_MAPPING)
+	return env
--- train.py
+++ train.py
@@ -1,126 +1,92 @@
 import os
 import torch
-from ppo import PPO
 import gymnasium as gym	# useful if using a wrapper for retro environments
 import retro
+import numpy as np
+import cv2
 import matplotlib.pyplot as plt
-from collections import deque
-
-# from cnn import Convolutional
-# from env import Environments
-# from env import ACTION_MAPPING
+import optuna
 
 # TODO: get rid of these eventually
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.callbacks import BaseCallback
-from stable_baselines3.common.env_checker import check_env
+
+from ppo import PPO
+from helpers import Callback, make_env
 
 print(f"PyTorch Version: {torch.__version__}, CUDA Version: {torch.version.cuda}, CUDA Enabled: {torch.cuda.is_available()}")
-
-# Custom Environment Wrapper
-# class RewardWrapper(gym.Wrapper):
-# 	def __init__(self, env):
-# 		super(RewardWrapper, self).__init__(env)
-# 		self.env.reset()
-# 		_, _, _, _, info = self.env.step(env.action_space.sample())
-# 		self.start_pos = info["x"]
-# 		self.start_score = info["score"]
-# 		self.start_rings = info["rings"]
-# 		self.start_lives = info["lives"]
-# 		self.env.reset()
-# 		self.progress = self.start_pos
-# 		self.curr_score = self.start_score
-# 		self.curr_rings = self.start_rings
-# 		self.curr_lives = self.start_lives
-
-# 	def step(self, action):
-# 		obs, _, terminated, truncated, info = self.env.step(action)
-# 		reward = min(max((info['x'] - self.progress), 0), 2)
-# 		# if self.progress != info['x']:
-# 		# 	print(f"progress: {self.progress}, x: {info['x']}")
-# 		self.progress = max(info['x'], self.progress)
-# 		reward += (info["score"] - self.curr_score) / 40
-# 		self.curr_score = info["score"]
-# 		if info["lives"] < 3:
-# 			return obs, -5, False, True, info
-# 		return obs, reward / 10, terminated, truncated, info
 	
-class RewardWrapper(gym.Wrapper):
-	def __init__(self, env):
-		super(RewardWrapper, self).__init__(env)
+ACTION_MAPPING = {
+	0: [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+	1: [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+	2: [0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0],
+	3: [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+}
 
-	def step(self, action):
-		obs, reward, terminated, truncated, info = self.env.step(action)
-		
-		position, velocity = obs
-		if velocity < 0:
-			velocity /= 2
-		velocity *= 100
-		reward = 0.5*(velocity**2) + 0.33*abs(position - (-0.5))
-		
-		return obs, reward, terminated, truncated, info
+def objective(trial):
+	gamma = trial.suggest_float('gamma', 0.9, 0.999)						# Discount factor
+	tau = trial.suggest_float('tau', 0.9, 0.999)							# Factor for trade-off of bias vs variance in GAE
+	beta = trial.suggest_float('beta', 0.001, 0.1)							# Entropy coefficient
+	lr = trial.suggest_loguniform('lr', 1e-5, 1e-3)							# Learning rate
+	n_steps = trial.suggest_categorical('n_steps', [256, 512, 1024])		# Steps before updating policy
+	batch_size = trial.suggest_categorical('batch_size', [256, 512, 1024])
+	epochs = trial.suggest_int('epochs', 3, 10)
+	clip_ratio = trial.suggest_float('clip_ratio', 0.1, 0.3)
+	# Create vectorized environment
+	params = {
+		"gamma": gamma,
+		"tau": tau,
+		"beta": beta,
+		"lr": lr,
+		"epochs": epochs,
+		"total_timesteps": 1000000,
+		"steps": n_steps,
+		"batch_size": batch_size,
+		"clip_ratio": clip_ratio,
+		"policy": "CnnPolicy",
+		"render": True,
+	}
+	num_envs = 10
+	env = SubprocVecEnv([make_env for _ in range(num_envs)])
+	model = PPO(
+		gamma=params["gamma"],
+		gae_lambda=params["tau"],
+		ent_coef=params["beta"],
+		learning_rate=params["lr"],
+		n_steps=params["steps"],
+		batch_size=params["batch_size"],
+		n_epochs=params["epochs"],
+		clip_range=params["clip_ratio"],
+		env=env,
+		policy=params["policy"],
+		device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	)
+	model.learn(total_timesteps=params["total_timesteps"], callback=Callback())
+	mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10, return_episode_rewards=True)
+	return np.mean(mean_reward)
 
-class FrameSkipper(gym.Wrapper):
-	def __init__(self, env, skip=4):
-		super(FrameSkipper, self).__init__(env)
-		self.skip = skip
-
-	def step(self, action):
-		reward = None
-		done = None
-		obs = None
-		info = {}
-		for i in range(self.skip):  # Add loop index
-			obs, reward, terminated, truncated, info = self.env.step(action)
-			reward = reward
-			if done:
-				break
-		return obs, reward, terminated, truncated, info
-
-	def reset(self, **kwargs):  # Accept additional keyword arguments
-		return self.env.reset(**kwargs)
-	
 def main():
-	# Sonic
 	params = {
-		"gamma": 0.99,				# Discount factor
-		"tau": 0.95,				# Factor for trade-off of bias vs variance in GAE
-		"beta": 0.1,				# Entropy coefficient
-		"epsilon": 0.2,				# Clipped surrogate objective
-		"lr": 0.001,				# Learning rate
+		"gamma": 0.75,				
+		"tau": 0.95,				
+		"beta": 0.33,				
+		"epsilon": 0.45,			
+		"lr": 0.003,				
 		"epochs": 10,
-		"total_timesteps": 100_000,
-		"steps": 1024,				# Steps before updating policy
-		"batch_size": 8,
-		"clip_ratio": 1,
+		"total_timesteps": 100_000_0,
+		"steps": 512,				
+		"batch_size": 1024,
+		"clip_ratio": 0.75,
+		"policy": "CnnPolicy",
 		"render": True,
 	}
-	# MountainCar
-	params = {
-		"gamma": 0.99,				# Discount factor
-		"tau": 0.95,				# Factor for trade-off of bias vs variance in GAE
-		"beta": 0.1,				# Entropy coefficient
-		"epsilon": 0.2,				# Clipped surrogate objective
-		"lr": 0.001,				# Learning rate
-		"epochs": 10,
-		"total_timesteps": 100_000,
-		"steps": 64,				# Steps before updating policy
-		"batch_size": 64,
-		"clip_ratio": 0.2,
-		"policy": "MlpPolicy",
-		"render": True,
-	}
-	# model = PPO(Convolutional, num_states=envs.num_states, num_actions=len(ACTION_MAPPING), params=params)
-	# model.learn(total_timesteps=100000)
-
-	env = gym.make('MountainCar-v0', render_mode='human')
-	# check_env(env, warn=True, skip_render_check=True)
-	env = RewardWrapper(env)
-	env = FrameSkipper(env, skip=1)
 	if not os.path.exists("sonic.zip"):
-		# Test stable_baselines x gymnasium
-		# Option 1: Single environment
-		# env = retro.make("SonicTheHedgehog-Genesis")
+		losses = []
+
+		# model.learn(total_timesteps=params["total_timesteps"], progress_bar=True, callback=Callback())
+		# Create vectorized environments
+		num_envs = 10
+		env = SubprocVecEnv([make_env() for _ in range(num_envs)])
 		model = PPO(
 			gamma=params["gamma"], 
 			gae_lambda=params["tau"], 
@@ -132,42 +98,20 @@ def main():
 			clip_range=params["clip_ratio"],
 			env=env, 
 			policy=params["policy"], 
-			device= torch.device("cuda" if torch.cuda.is_available() else "cpu"))
-		# Track losses
-		losses = []
-		# Called whenever a step is made
-		class Callback(BaseCallback):
-			def __init__(self, verbose=0):
-				super(Callback, self).__init__(verbose)
-				losses = []
-
-			def _on_step(self) -> bool:
-				if 'rewards' in self.locals and self.locals['rewards'] > 0:
-					print(f"Reward on {self.num_timesteps}: {self.locals['rewards']}")
-				elif 'rewards' in self.locals and self.locals['rewards'] < 0:
-					print(f"Loss on {self.num_timesteps}: {self.locals['rewards']}")
-				return True
+			device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+		)
+		model = PPO(params["policy"], env, verbose=1)
 		model.learn(total_timesteps=params["total_timesteps"], progress_bar=True, callback=Callback())
-		# Saving losses plot, not working for some reason
+
 		plt.plot(losses)
 		plt.xlabel('Steps')
 		plt.ylabel('Loss')
 		plt.title('Loss vs Timesteps')
 		plt.savefig('loss_vs_timesteps.png')
-		# Option 2: Multiprocessing
-		# def make_env():
-		# 	def _init():
-		# 		env = retro.make(game='SonicTheHedgehog-Genesis')
-		# 		return env
-		# 	return _init
-		# # Create vectorized environments
-		# num_envs = 4
-		# env = SubprocVecEnv([make_env() for _ in range(num_envs)])
-		# model = PPO("MlpPolicy", env, verbose=1)
-		# model.learn(total_timesteps=2e5, progress_bar=True)
 		model.save("sonic")
+
+	env = make_env()
 	model = PPO.load("sonic", env=env)
-	# mean_reward, std_reward = evaluate_policy(model, model.get_env(), n_eval_episodes=10)
 	vec_env = model.get_env()
 	obs = vec_env.reset()
 	done = False
@@ -176,5 +120,6 @@ def main():
 		obs, reward, done, info = vec_env.step(action)
 		vec_env.render()
 	env.close()
+
 if __name__ == "__main__":
 	main()
