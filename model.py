0fb4bdd1ff4352345b4a56efca969e848a8e1b7c
Iemontine <darrollsaddi@gmail.com>
2024-06-03 11:44:43 -0700

    fixed model.py

--- model.py
+++ model.py
@@ -4,19 +4,20 @@ from stable_baselines3.common.vec_env import VecFrameStack, SubprocVecEnv
 from train import params
 
 def main():
-	env = SubprocVecEnv([make_env(params["level"], skip=params["frame_skip"], obs_dim=params["observation_dimension"], pass_num=2) for _ in range(5)])
-	env = VecFrameStack(env, n_stack=params["frame_stack"])
-	model = PPO.load("sonic2", env=env)
-	vec_env = model.get_env()
-	obs = vec_env.reset()
-	done = False
-
-	while not done:
-		action, states = model.predict(obs)
-		obs, reward, _, info = vec_env.step(action)
-		done = info[0]['act'] == 1
-		vec_env.render()
-	env.close()
+	try:
+		env = SubprocVecEnv([make_env(params["level"], skip=params["frame_skip"], obs_dim=params["observation_dimension"], pass_num=params["pass_num"]) for _ in range(params["num_envs"])])
+		env = VecFrameStack(env, n_stack=params["frame_stack"])
+		model = PPO.load("sonic3", env=env)
+		vec_env = model.get_env()
+		obs = vec_env.reset()
+		done = False
+		while not done:
+			action, _ = model.predict(obs)
+			obs, reward, done, info = vec_env.step(action)
+			vec_env.render()
+		env.close()
+	except Exception as e:
+		print(f"{e}")
 
 if __name__ == "__main__":
 	main()
