c9627b9edb865c265e490808fca574f9b79597c7
Iemontine <darrollsaddi@gmail.com>
2024-06-11 17:43:12 -0700

    moved graphing, deleted "old" folder, custom numenvs in model.py

--- model.py
+++ model.py
@@ -3,17 +3,22 @@ from ppo import PPO
 from stable_baselines3.common.vec_env import VecFrameStack, SubprocVecEnv
 from train import params
 
+NUM_ENVS = 3
+
 def main():
-    env = SubprocVecEnv([make_env(params["level"], skip=params["frame_skip"], obs_dim=params["observation_dimension"], pass_num=params["pass_num"]) for _ in range(params["num_envs"])])
+    env = SubprocVecEnv([make_env(params["level"], skip=params["frame_skip"], obs_dim=params["observation_dimension"], pass_num=params["pass_num"]) for _ in range(NUM_ENVS)])
     env = VecFrameStack(env, n_stack=params["frame_stack"])
     model = PPO.load("sonic3", env=env)
     vec_env = model.get_env()
     obs = vec_env.reset()
-    done = [False] * env.num_envs
-    while not any(done):
+    all_done = [False] * NUM_ENVS
+    while all_done != [True, True, True]:
         action, _ = model.predict(obs)
         obs, reward, done, info = vec_env.step(action)
-        print(reward)
+        for i in range(NUM_ENVS):
+            if done[i]:
+                all_done[i] = True
+        print(all_done)
         vec_env.render()
     env.close()
 
--- old/OpenAI/README.md
+++
@@ -1,3 +0,0 @@
-# Proximal Policy Optimization
-
-Project was built on WLS - Ubuntu 22.04 LTS, using Python 3.7.17
